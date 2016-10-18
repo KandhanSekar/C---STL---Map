a44ff4b6f3bf74aa87b125030294ffa2f805b06c
KandhanSekar <kandhan3694@gmail.com>
2016-10-17 19:58:17 -0700

    stuff

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,21 +7,36 @@
 using namespace std;
 //map with string as key (name here) and value is a vector storing two marks of the string
 map<string, vector<int>>a;
+
+void findanddel(string ar)
+{
+	map<string,vector<int>>::iterator iter=a.find(ar);
+	if (iter != a.end())
+	{
+		a.erase(ar);
+	}
+	
+}
+
+
+
 void insert(string nam, int b, int c)
 {
-	cout << "here 1";
+	//cout << "here 1";
+	//vector to be pushed as second para
 	vector<int>vec;
 	vec.push_back(b);
 	vec.push_back(c);
 	//first m1 is pushed , then m2
 	a.insert(make_pair(nam, vec));
+	//inserting in the map
 }
 int main()
 {
 	
-	cout << "MAP Implementation";
+	cout << "\n MAP Implementation \n";
 	
-	cout << "Operations \n 1. Insert new record \n 2.Display all ";
+	cout << "Operations \n 1. Insert new record \n 2.Display all\n 3. Delete this record ";
 	int opt;
 	cin >> opt;
 	switch (opt) {
@@ -39,17 +54,32 @@ int main()
 	}
 	case 2:
 	{
-		for (map < string, vector<int>>::iterator bow = a.begin(); bow != a.end(); ++bow)
+		//simple map iterator
+		for (map < string, vector<int>>::iterator itern = a.begin(); itern != a.end(); ++itern)
 		{
-			cout << "\n printing first ba!";
-			cout << "\n" << (*bow).first << "=>";
-			for (vector<int>::iterator b = (*bow).second.begin(); b != (*bow).second.end(); ++b)
+			cout << "\n" << (*itern).first << "=>";
+			for (vector<int>::iterator b = (*itern).second.begin(); b != (*itern).second.end(); ++b)
 				cout << *b << "\t";
 		}
 	}
 		break;
+
+
+
+	case 3:
+	{
+		cout << "\n What do u want to delete";
+		string h;
+		cin >> h;
+		findanddel(h);
+
+
+	}
+
+	break;
 	}
 	//Operations\n 2. Delete record \n 3. Modify record \n 4. Find and display \n 5
+
 	main();
 	return 0;
 
