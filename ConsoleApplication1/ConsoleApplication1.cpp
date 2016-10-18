3cd5cba29f99fe2c74d2ce8810d0412896fa56bf
KandhanSekar <kandhan3694@gmail.com>
2016-10-17 18:41:34 -0700

    hb

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include<map>
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main()
+{
+	map<string,vector<int,int>>a;
+	cout << "MAP Implementation";
+	vector<int,int> b;
+	cout << "Operations \n 1. Insert new record \n 2. Delete record \n 3. Modify record \n 4. Find and display \n 5.   ";
+
+    return 0;
+}
+
